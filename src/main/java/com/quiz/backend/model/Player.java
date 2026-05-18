cdfba04f31d4b398fd609d0a487dee675c0f245a
Max Burenko <maxburenko@gmail.com>
2026-05-18 11:32:39 +0600

    feat: add JPA persistence support

--- src/main/java/com/quiz/backend/model/Player.java
+++ src/main/java/com/quiz/backend/model/Player.java
@@ -1,9 +1,18 @@
 package com.quiz.backend.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Table(name = "players")
+@Entity
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+
     private String sessionId; // ID сессии в вебсокете
     private String nickname;
     private int score;
