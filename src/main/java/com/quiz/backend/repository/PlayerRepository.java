9f543afd029ae92f93e1fb1663cd2854626e6b39
Max Burenko <maxburenko@gmail.com>
2026-05-19 15:26:21 +0600

    feat: add PlayerRepository

---
+++ src/main/java/com/quiz/backend/repository/PlayerRepository.java
@@ -0,0 +1,11 @@
+package com.quiz.backend.repository;
+
+import com.quiz.backend.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    Optional<Player> findBySessionId(String sessionId);
+}
