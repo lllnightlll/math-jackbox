9f345bc5b1015d48110d786dfdca89dd388dced7
Max Burenko <maxburenko@gmail.com>
2026-05-19 15:17:27 +0600

    feat: convert to JPA entity

--- src/main/java/com/quiz/backend/model/Question.java
+++ src/main/java/com/quiz/backend/model/Question.java
@@ -1,15 +1,30 @@
 package com.quiz.backend.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
-//ЗАГЛУШКА ВМЕСТО БД
 @Data
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "questions")
 public class Question {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String text;
+
+    @ElementCollection()
+    @CollectionTable(name = "question_option", joinColumns = @JoinColumn(name = "question_id"))
+    @Column(name = "option_text")
     private List<String> options;
+
+    @Column(name = "correct_option_index", nullable = false)
     private int correctOptionIndex;
 }
