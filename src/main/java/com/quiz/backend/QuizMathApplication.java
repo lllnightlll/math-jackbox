60ecc416e756df294ad4ff7958460625a4af105d
Max Burenko <maxburenko@gmail.com>
2026-05-19 15:23:13 +0600

    feat: enable DataSource auto-configuration

--- src/main/java/com/quiz/backend/QuizMathApplication.java
+++ src/main/java/com/quiz/backend/QuizMathApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
+
 public class QuizMathApplication {
 
     public static void main(String[] args) {
